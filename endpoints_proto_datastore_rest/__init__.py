18daf49931ee04d0433a299ce56eb804db298524
David Chen <lucemia@gmail.com>
2014-06-09 18:48:54 +0800

    refactor endpoints rest

--- endpoints_proto_datastore_rest/__init__.py
+++ endpoints_proto_datastore_rest/__init__.py
@@ -4,101 +4,98 @@ from protorpc import remote
 # TODO: need to refine method name
 # TODO: allow user to override/insert method
 
+def _default_insert(self, model):
+    model.put()
+    return model
+
+def _default_list(self, query):
+    return query
+
+def _default_update(self, model):
+    model.put()
+    return model
+
+def _default_get(self, model):
+    return model
+
+def _default_delete(self, model):
+    return model.key.delete()
+
+def bind(func, name, doc):
+    func.__name__ = name
+    func.__doc__ = doc
+    return func
+
 class EndpointRestBuilder(object):
     def __init__(self, cls):
         self.cls = cls
+        self.name = self.cls.__name__.lower()
 
-    @property
-    def name(self):
-        return self.cls.__name__.lower()
+        self._methods = {}
 
-    def build_model_insert(self, name, doc):
-        def func(self, model):
-            model.put()
-            return model
-        func.__name__ = name
-        func.__doc__ = doc
-        return func
+        self.set_insert(bind(_default_insert, "insert", "%s insert" % self.name))
+        self.set_delete(bind(_default_delete, "delete", "%s delete" % self.name))
+        self.set_update(bind(_default_update, "update", "%s update" % self.name))
+        self.set_get(bind(_default_get, "get", "%s get" % self.name))
+        self.set_list(bind(_default_list, "list", "%s list" % self.name))
 
-    def build_model_list(self, name, doc):
-        def func(self, query):
-            return query
+    def set_method(self, func, method_name, **kwargs):
+        self._methods[method_name] = self.cls.method(**kwargs)(func)
 
-        func.__name__ = name
-        func.__doc__ = doc
-        return func
+    def set_query_method(self, func, method_name, **kwargs):
+        self._methods[method_name] = self.cls.query_method(**kwargs)(func)
 
-    def build_model_update(self, name, doc):
-        def func(self, model):
-            model.put()
-            return model
-        func.__name__ = name
-        func.__doc__ = doc
-        return func
+    def set_get(self, func):
+        self.set_method(
+            func,
+            "get",
+            path="%s/{id}"% self.name,
+            http_method="GET",
+            name="%s.get" % self.name
+        )
 
-    def build_model_get(self, name, doc):
-        def func(self, model):
-            return model
-        func.__name__ = name
-        func.__doc__ = doc
-        return func
+    def set_delete(self, func):
+        self.set_method(
+            func,
+            "delete",
+            path="%s/{id}" % self.name,
+            http_method="DELETE",
+            name="%s.delete" % self.name,
+        )
 
-    def build_model_delete(self, name, doc):
-        def func(self, model):
-            model.key.delete()
-        func.__name__ = name
-        func.__doc__ = doc
-        return func
+    def set_update(self, func):
+        self.set_method(
+            func,
+            "update",
+            path="%s/{id}"% self.name,
+            http_method="PUT",
+            name="%s.update" % self.name
+        )
 
+    def set_insert(self, func):
+        self.set_method(
+            func,
+            "insert",
+            path="%s" % self.name,
+            http_method="POST",
+            name="%s.insert" % self.name
+        )
 
-    def gen_model_list(self):
-        return self.cls.query_method(
+    def set_list(self, func):
+        self.set_query_method(
+            func,
+            "list",
             path='%s/list' % self.name ,
             http_method='GET',
             name="%s.list" % self.name,
             query_fields=('limit', 'pageToken')
-        )(self.build_model_list(self.name, '%s list' % self.name))
+        )
 
-    def gen_model_insert(self):
-        return self.cls.method(
-            path="%s" % self.name,
-            http_method="POST",
-            name="%s.insert" % self.name
-        )(self.build_model_insert(self.name, '%s insert' % self.name))
-
-    def gen_model_update(self):
-        return self.cls.method(
-            path="%s/{id}"% self.name,
-            http_method="PUT",
-            name="%s.update" % self.name
-        )(self.build_model_update(self.name, '%s update' % self.name))
-
-    def gen_model_get(self):
-        return self.cls.method(
-            path="%s/{id}"% self.name,
-            http_method="GET",
-            name="%s.get" % self.name,
-        )(self.build_model_get(self.name, '%s get' % self.name))
-
-    def gen_model_delete(self):
-        return self.cls.method(
-            path="%s/{id}" % self.name,
-            http_method="DELETE",
-            name="%s.delete" % self.name,
-        )(self.build_model_delete(self.name, '%s delete' % self.name))
-
-
-    def build(self, name, version, description):
-        return endpoints.api(name=name.lower(), version=version, description=description)(
+    def build(self, api_name, **kwargs):
+        return endpoints.api(**kwargs)(
             type(
-                name,
+                api_name,
                 (remote.Service, ),
-                {
-                    "%s_insert" % self.name: self.gen_model_insert(),
-                    "%s_list" % self.name: self.gen_model_list(),
-                    "%s_get" % self.name: self.gen_model_get(),
-                    "%s_update"% self.name: self.gen_model_update(),
-                    "%s_delete"%self.name: self.gen_model_delete()
-                }
+                self._methods
             )
         )
